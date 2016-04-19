26012369f191fa73097198e94999e8943a789666
NuriaValls <nvallscanudas@hotmail.com>
2016-04-19 08:14:36 +0200

    Base de dades i interifice acabades!!

--- src/model/MTMainServer.java
+++ src/model/MTMainServer.java
@@ -12,22 +12,6 @@ public class MTMainServer {
 		ResultSet consulta;
 		ConectorDB conn = new ConectorDB("root", "", "memotournamentdb", 3306);
 		conn.connect();
-		//conn.insertUser("Nick", "solsona");
-		//conn.insertUser("Ruru", ":3");
-		conn.insertGame("Nick", "memoria", 112);
-		//conn.deleteUser("Rafa");
-		//conn.updateScore("Ruru", 55);
-		consulta = conn.selectGames("Nick");
-		
-		try {
-			while (consulta.next())
-			{
-				System.out.println (consulta.getObject("name") + " " + consulta.getObject("mode")+ " " + consulta.getObject("score"));
-			}
-		} catch (SQLException e) {
-			System.out.println("Problema al recuperar les dades...");
-		}
-		
 		conn.disconnect();
 	}
 }
