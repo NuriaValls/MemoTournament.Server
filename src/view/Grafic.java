9d1dd2761f77bbc7e15883afc93b0568f2c5f6f9
SergiGandul <sergigandul@gmail.com>
2016-05-19 22:49:16 +0200

    bhj

--- src/view/Grafic.java
+++ src/view/Grafic.java
@@ -1,12 +1,15 @@
 package view;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+import model.UserRanking;
+
 public class Grafic extends JPanel{
-	
-	
+	private ArrayList<UserRanking> allUsers;
+	private int typeGraph;
 	public Grafic(){
 		
 	}
@@ -14,9 +17,16 @@ public class Grafic extends JPanel{
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		
-		g.drawLine(30, 20, 200, 20);
-        
+		//pintar eixos.
+		g.drawLine(0, 20, 0, 500);
+        g.drawLine(0, 300, 0, 300);
+        if (typeGraph == 1){
+        	for(int i=0;i<allUsers.size();i++){
+        		
+        	}
+        }else{
+        	
+        }
 		
 	}
 
