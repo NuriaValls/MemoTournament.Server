f1bdcc02d1661c55da584afed37c070f2aef095b
SergiGandul <sergigandul@gmail.com>
2016-05-21 23:35:24 +0200

    vgh

--- src/view/Grafic.java
+++ src/view/Grafic.java
@@ -82,6 +82,7 @@ public class Grafic extends JPanel{
         	int scale = maxPunctuation/210;
         	System.out.println(scale);
         	int ix = 30;
+        	//quan maxPunctuation>240 no pinta dins del panell
         	for(int j=0;j<score1.length-1;j++){
         		g.drawLine(ix,240-(score1[j]*scale),ix+(200/score1.length),240-(score1[j+1]*scale));
         		ix= ix+(200/score1.length);
@@ -96,41 +97,47 @@ public class Grafic extends JPanel{
      //aquesta part es igual que l'anterior pel cas typeGraph=2
         }else{
         	int maxPunctuation = 0;
-        	for(int i=0;i<score2.length;i++){
-        		if (score2[i]>maxPunctuation){
-        			maxPunctuation = score2[i];
+        	for(int i=0;i<score1.length;i++){
+        		if (score1[i]>maxPunctuation){
+        			maxPunctuation = score1[i];
         		}
         	}
-        	
+        	System.out.println(maxPunctuation);
         	//Pintar numeros eix Y:
         	int incrementy=0;
-        	incrementy = maxPunctuation/5;
-        	g.drawString("0", 0, 0);
-        	int y = 0;
-        	for(int i=0;i<score1.length;i++){
-            	g.drawString(String.valueOf(incrementy),0,y + 50);
-            	y=y+100;
-            	incrementy = incrementy + incrementy;
+        	int incrementp = maxPunctuation/4;
+        	
+        	int y = 240;
+        	int n=0;
+        	for(int i=0;i<score1.length-1;i++){
+       			if (n<4){
+       				g.drawString(String.valueOf(incrementy + incrementp),0,y - 54);
+                  	y=y-54;
+                  	n++;
+                  	incrementy = incrementy + incrementp;
+       			}
             }
         	
         	
         	//Pintar numeros eix X:
         	
-        	int incrementx=0;
-        	incrementx = score1.length/5;
-        	g.drawString("0", 0, 0);
-        	int eixX = 0;
-        	for(int i=0;i<score1.length;i++){
-            	g.drawString(String.valueOf(incrementx),0,eixX+ 100);
-            	eixX=eixX+100;
-            	incrementx = incrementx + incrementx;
+        	int partides = score1.length;
+        	int incrementP = 210/partides;
+        	int aux = incrementP;
+        	System.out.println(incrementP);
+        	for(int i=1;i<score1.length-1;i++){
+            	g.drawString(String.valueOf(i),incrementP+15,252);
+            	incrementP=incrementP+aux;
             }
         	
-        	int scale = 500/maxPunctuation;
-        	int ix = 0;
-        	for(int i=0;i<score2.length-1;i++){
-        		g.drawLine(ix,score2[i]*scale-500,ix+30,score2[i+1]*scale-500);
-        		ix= ix+30;
+        	
+        	//Pintar puntuacions
+        	int scale = maxPunctuation/210;
+        	System.out.println(scale);
+        	int ix = 30;
+        	for(int j=0;j<score1.length-1;j++){
+        		g.drawLine(ix,240-(score1[j]*scale),ix+(200/score1.length),240-(score1[j+1]*scale));
+        		ix= ix+(200/score1.length);
         	}
         }
 		
