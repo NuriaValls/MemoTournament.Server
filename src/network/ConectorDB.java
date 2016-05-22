30a1ed298b0371f455b559ec9765215de309f2b8
juliagrifols <grifols.julia@gmail.com>
2016-05-22 18:15:18 +0200

    fgh

--- src/network/ConectorDB.java
+++ src/network/ConectorDB.java
@@ -27,7 +27,7 @@ public class ConectorDB {
     public void connect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = (Connection) DriverManager.getConnection(url, "root", "");
+            conn = (Connection) DriverManager.getConnection(url, "root", "root");
             if (conn != null) {
                 System.out.println("Conexi� a base de dades "+url+" ... Ok");
             }
